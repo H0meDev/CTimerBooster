267e46a44cefb66af2d7733a9c7979ccaff98f0d
蔡亮 <362397585@qq.com>
2015-03-20 16:44:01 +0800

    Update CTimerBooster.h
    1、增加参数支持
    2、增加重复执行

--- CTimerBooster.h
+++ CTimerBooster.h
@@ -10,11 +10,24 @@
 
 @interface CTimerBooster : NSObject
 
-// 开始频率Timer计时,以0.001s发生一次
+// 开始频率Timer计时
 + (void)start;
 
+// 添加一个接收目标, 不重复执行
++ (void)addTarget:(id)target sel:(SEL)selector time:(NSTimeInterval)time;
+
 // 添加一个接收目标
-+ (void)addTarget:(id)target sel:(SEL)selector time:(NSUInteger)time;
++ (void)addTarget:(id)target sel:(SEL)selector time:(NSTimeInterval)time repeat:(BOOL)repeat;
+
+// 添加一个带参接收目标
++ (void)addTarget:(id)target sel:(SEL)selector param:(id)parameters time:(NSTimeInterval)time;
+
+// 添加一个带参接收目标
++ (void)addTarget:(id)target
+              sel:(SEL)selector
+            param:(id)parameters
+             time:(NSTimeInterval)time
+           repeat:(BOOL)repeat;
 
 // 移除一个接收目标
 + (void)removeTarget:(id)target sel:(SEL)selector;
